b8f233376822b44785162fc3d674c84ef8d630c6
Mikael Salmela <mikael@ronati.com>
2022-12-05 19:30:30 +0400

    refactored ChatGPT code

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,106 +19,47 @@ function SecurePasswordGenerator(): JSX.Element {
       return String.fromCharCode((c.charCodeAt(0) % 26) + 97);
     });
 
+    const index = 6;
+
+    function getRandomIndexes(): [number, number] {
+      // Generate the first index
+      const randomIndexOne = Math.floor(Math.random() * index);
+
+      // Generate the second index
+      let randomIndexTwo = Math.floor(Math.random() * index);
+
+      // Avoid selecting the same index for both
+      while (randomIndexOne === randomIndexTwo) {
+        randomIndexTwo = Math.floor(Math.random() * index);
+      }
+
+      return [randomIndexOne, randomIndexTwo];
+    }
+
+    // Get two random indexes
+    const [randomIndexOne, randomIndexTwo] = getRandomIndexes();
+
     // Split the password string into three parts, each 6 characters long
-    const firstPart = passwordLetters.substring(0, 6);
-    const secondPart = passwordLetters.substring(6, 12);
-    const thirdPart = passwordLetters.substring(12);
-
-    // Select firstPart, secondPart, or thirdPart randomly
-    const selectedPart1 = Math.floor(Math.random() * 3);
-
-    // Random index for firstPart
-    const firstPartIndex = Math.floor(Math.random() * firstPart.length);
+    const firstPart = passwordLetters.substring(index - index, index);
+    const secondPart = passwordLetters.substring(index, index + index);
+    const thirdPart = passwordLetters.substring(index + index);
 
     // Upper case letter based on firstPartIndex
-    const upperCase1 = firstPart[firstPartIndex].toUpperCase();
+    const upperCase1 = firstPart[randomIndexOne].toUpperCase();
 
-    // Random index for secondPart
-    const secondPartIndex = Math.floor(Math.random() * secondPart.length);
+    const modifiedFirstPart =
+      firstPart.substring(0, randomIndexOne) +
+      upperCase1 +
+      firstPart.substring(randomIndexOne + 1);
 
-    // Upper case letter based on secondPartIndex
-    const upperCase2 = secondPart[secondPartIndex].toUpperCase();
+    const modifiedSecondPart =
+      secondPart.substring(0, randomIndexTwo) +
+      randomIndexTwo +
+      secondPart.substring(randomIndexTwo + 1);
 
-    // Random index for thirdPart
-    const thirdPartIndex = Math.floor(Math.random() * secondPart.length);
-
-    // Upper case letter based on thirdPartIndex
-    const upperCase3 = thirdPart[thirdPartIndex].toUpperCase();
-
-    // Bucket for the first modified password
-    let modifiedPart1 = '';
-
-    // Select the first part of the password
-    switch (selectedPart1) {
-      case 0:
-        // Code to be executed if selectedPart1 has a value of 0
-        modifiedPart1 =
-          firstPart.substring(0, firstPartIndex) +
-          upperCase1 +
-          firstPart.substring(firstPartIndex + 1);
-        //console.log(modifiedPart1);
-        break;
-      case 1:
-        // Code to be executed if selectedPart1 has a value of 1
-        modifiedPart1 =
-          secondPart.substring(0, secondPartIndex) +
-          upperCase2 +
-          secondPart.substring(secondPartIndex + 1);
-        //console.log(modifiedPart1);
-        break;
-      case 2:
-        // Code to be executed if selectedPart1 has a value of 2
-        modifiedPart1 =
-          thirdPart.substring(0, thirdPartIndex) +
-          upperCase3 +
-          thirdPart.substring(thirdPartIndex + 1);
-        //console.log(modifiedPart1);
-        break;
-      default:
-        // Code to be executed if selectedPart1 has a value other than 0, 1, or 2
-        break;
-    }
-
-    // Select the second part of the password
-    let selectedPart2 = Math.floor(Math.random() * 3);
-
-    // Avoid selecting the same part already selected for the first part
-    while (selectedPart2 === selectedPart1) {
-      selectedPart2 = Math.floor(Math.random() * 3);
-    }
-
-    // Bucket for the second modified password
-    let modifiedPart2 = '';
-
-    switch (selectedPart2) {
-      case 0:
-        modifiedPart2 =
-          firstPart.substring(0, firstPartIndex) +
-          firstPartIndex +
-          firstPart.substring(firstPartIndex + 1);
-        //console.log(modifiedPart2);
-        break;
-      case 1:
-        modifiedPart2 =
-          secondPart.substring(0, secondPartIndex) +
-          secondPartIndex +
-          secondPart.substring(secondPartIndex + 1);
-        //console.log(modifiedPart2);
-        break;
-      case 2:
-        modifiedPart2 =
-          thirdPart.substring(0, thirdPartIndex) +
-          thirdPartIndex +
-          thirdPart.substring(thirdPartIndex + 1);
-        //console.log(modifiedPart2);
-        break;
-      default:
-        break;
-    }
-    const passwordParts = [modifiedPart1, modifiedPart2, thirdPart];
+    const passwordParts = [modifiedFirstPart, modifiedSecondPart, thirdPart];
     const shuffledPasswordParts = passwordParts.sort(() => Math.random() - 0.5);
     const password = shuffledPasswordParts.join('-');
-    // console.log(😎);
     setPassword(password);
     return password;
   }
